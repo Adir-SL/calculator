c0c8a6be8cd1043be6375a3a4836144faa8d60ea
Adir <slutzkis@gmail.com>
2026-01-13 11:06:02 +0200

    new calc buttons

--- calc.js
+++ calc.js
@@ -1,3 +1,23 @@
 function calcButton(a) {
   document.getElementById("result").innerText += a;
 }
+
+function addPoint() {
+  document.getElementById("result").innerText += ".";
+}
+
+function calcMinus() {
+  document.getElementById("result").innerText += "-";
+}
+
+function calcPlus() {
+  document.getElementById("result").innerText += "+";
+}
+
+function calcMultiply() {
+  document.getElementById("result").innerText += "*";
+}
+
+function calcDevide() {
+  document.getElementById("result").innerText += "/";
+}
